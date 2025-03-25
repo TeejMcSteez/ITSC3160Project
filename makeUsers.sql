fd55c6183c7c45a959156b1a3533a812937f65fc
Lansina <129553714+Lansina@users.noreply.github.com>
2025-03-25 18:04:46 -0400

    Update makeUsers.sql
    Just removed the previous comments about TIMESTAMP. pls lmk if I can add anything to both tables.

--- makeUsers.sql
+++ makeUsers.sql
@@ -4,38 +4,7 @@ CREATE TABLE USERS IF NOT EXISTS (
     ROLE TEXT NOT NULL,
     EXPIRY TIMESTAMP NOT NULL,
 
-    
-    
-    /*
-                        Not sure if we
-    want to use timestamp or just the time in x reading as a number
-                Read below for more info about the issue.
-    */
 
-    /*
-    Currently timestamp can be accessed like this:
-    SELECT EXPIRY FROM USERS WHERE TIMESTAMP(NOW()) > EXPIRY;
-
-    This will return all the rows where the expiry time has passed.
-
-    timestamp takes two arguments
-
-    (YYYY-MM-DD)
-    and
-    (HR:MIN:SEC)
-
-    so to set the expiry time to 1 hour from now, you can do this:
-    INSERT INTO USERS VALUES (1, "supersecrettoken1", "USER", TIMESTAMP(NOW(), INTERVAL 1 HOUR));
-
-    However this method has technical debt as you have to learn the timestamp
-    syntax and it is not as readable as just using a number. But more flexible.
-
-    I like the idea of using Timestamp since it's more of a realistic, flexible approach tbh.
-
-    
-
-    
-    */
     FOREIGN KEY (CLIENT_ID) REFERENCES ADMIN(MASTER_TOKEN),
 );
 
